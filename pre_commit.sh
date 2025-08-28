1a7d87fd152e5f16d104192d3c3e299f32a011c7
hedgeg0d <werstak192@gmail.com>
2025-08-28 16:46:23 +0300

    fix incremet for arrs

--- pre_commit.sh
+++ pre_commit.sh
@@ -3,5 +3,7 @@
 # Use this script to format your code before every commit, and ensure that all tests are passed, and nothing broke
 zig fmt .
 ./run_tests.sh
+./zig-out/bin/zlang examples/brainfuck.zl 
+echo "++++++[>++++++++<-]>." | ./output 
 cd examples/multi_file_c
 ./build.sh
