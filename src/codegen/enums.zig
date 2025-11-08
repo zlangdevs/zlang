015e07d23db716116f42163767f411c6cf0ec33b
Hedegod <hedgegod@stepbro.wtf>
2025-11-08 22:03:52 +0300

    enum refactoring

---
+++ src/codegen/enums.zig
@@ -0,0 +1,109 @@
+const std = @import("std");
+const ast = @import("../parser/ast.zig");
+const errors = @import("../errors.zig");
+const utils = @import("utils.zig");
+const structs = @import("structs.zig");
+
+const c_bindings = @import("c_bindings.zig");
+const c = c_bindings.c;
+
+const CodeGenerator = @import("llvm.zig").CodeGenerator;
+
+/// Generate enum declaration with global constants
+pub fn generateEnumDeclaration(self: *CodeGenerator, enum_decl: ast.EnumDecl) errors.CodegenError!void {
+    var current_value: i32 = 0;
+    for (enum_decl.values.items) |enum_value| {
+        var value: i32 = current_value;
+        if (enum_value.value) |expr| {
+            const const_value = try self.generateExpression(expr);
+            if (c.LLVMIsConstant(const_value) == 0) {
+                return errors.CodegenError.TypeMismatch;
+            }
+            const int_value = c.LLVMConstIntGetSExtValue(const_value);
+            value = @intCast(int_value);
+        }
+        const enum_const = c.LLVMConstInt(c.LLVMInt32TypeInContext(self.context), @as(c_ulonglong, @intCast(value)), 0);
+        const enum_name_z = try std.fmt.allocPrint(self.allocator, "{s}_{s}", .{ enum_decl.name, enum_value.name });
+        defer self.allocator.free(enum_name_z);
+        const enum_name_z_null = utils.dupeZ(self.allocator, enum_name_z);
+        defer self.allocator.free(enum_name_z_null);
+        const global_var = c.LLVMAddGlobal(self.module, c.LLVMInt32TypeInContext(self.context), enum_name_z_null.ptr);
+        c.LLVMSetInitializer(global_var, enum_const);
+        c.LLVMSetLinkage(global_var, c.LLVMExternalLinkage);
+        c.LLVMSetGlobalConstant(global_var, 1);
+
+        const var_info = structs.VariableInfo{
+            .value = @ptrCast(global_var),
+            .type_ref = @ptrCast(c.LLVMInt32TypeInContext(@ptrCast(self.context))),
+            .type_name = "i32",
+            .is_const = true,
+        };
+        try self.variables.put(utils.dupe(u8, self.allocator, enum_name_z), var_info);
+        try self.variables.put(utils.dupe(u8, self.allocator, enum_value.name), var_info);
+
+        current_value = value + 1;
+    }
+}
+
+/// Process global enums in function nodes
+pub fn processGlobalEnums(self: *CodeGenerator, func_node: *ast.Node) errors.CodegenError!void {
+    switch (func_node.data) {
+        .function => |func| {
+            for (func.body.items) |stmt| {
+                try findAndProcessEnums(self, stmt);
+            }
+        },
+        else => {},
+    }
+}
+
+/// Recursively find and process enum declarations in statements
+pub fn findAndProcessEnums(self: *CodeGenerator, node: *ast.Node) errors.CodegenError!void {
+    switch (node.data) {
+        .enum_decl => |enum_decl| {
+            try generateEnumDeclaration(self, enum_decl);
+        },
+        .if_stmt => |if_stmt| {
+            for (if_stmt.then_body.items) |stmt| {
+                try findAndProcessEnums(self, stmt);
+            }
+            if (if_stmt.else_body) |else_body| {
+                for (else_body.items) |stmt| {
+                    try findAndProcessEnums(self, stmt);
+                }
+            }
+        },
+        .for_stmt => |for_stmt| {
+            for (for_stmt.body.items) |stmt| {
+                try findAndProcessEnums(self, stmt);
+            }
+        },
+        .c_for_stmt => |c_for_stmt| {
+            for (c_for_stmt.body.items) |stmt| {
+                try findAndProcessEnums(self, stmt);
+            }
+        },
+        else => {},
+    }
+}
+
+/// Try to load an enum value from a qualified identifier (e.g., Color.RED)
+/// Returns null if not an enum value
+pub fn tryLoadEnumValue(self: *CodeGenerator, qual_id: ast.QualifiedIdentifier) errors.CodegenError!?c.LLVMValueRef {
+    if (qual_id.base.data != .identifier) {
+        return null;
+    }
+
+    const base_name = qual_id.base.data.identifier.name;
+    const enum_var_name = try std.fmt.allocPrint(self.allocator, "{s}_{s}", .{ base_name, qual_id.field });
+    defer self.allocator.free(enum_var_name);
+
+    if (CodeGenerator.getVariable(self, enum_var_name)) |enum_var_info| {
+        const load_instr = c.LLVMBuildLoad2(self.builder, @ptrCast(enum_var_info.type_ref), @ptrCast(enum_var_info.value), "enum_value");
+        const alignment = self.getAlignmentForType(@ptrCast(enum_var_info.type_ref));
+        c.LLVMSetAlignment(load_instr, alignment);
+        return load_instr;
+    }
+
+    return null;
+}
