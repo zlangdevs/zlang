abc2e37c99311da2c721a642a99bc79eae4aa8df
Hedegod <hedgegod@stepbro.wtf>
2025-12-22 22:45:09 +0300

    better llvm-tools search

--- src/interpreter.zig
+++ src/interpreter.zig
@@ -1,10 +1,24 @@
 const std = @import("std");
+const llvm_tools = @import("llvm_tools.zig");
 
 pub fn runWithLLI(allocator: std.mem.Allocator, ll_file: []const u8, program_args: []const []const u8) !u8 {
+    const lli_tool = try llvm_tools.getLLVMToolPath(allocator, .lli);
+    if (lli_tool == null) {
+        std.debug.print("Error: lli (LLVM interpreter) not found.\n", .{});
+        std.debug.print("Tried: lli-20, lli-19, lli-18, ..., lli\n", .{});
+        std.debug.print("Please install LLVM tools.\n", .{});
+        return error.LLINotFound;
+    }
+    defer if (lli_tool) |tool| {
+        if (!std.mem.eql(u8, tool, "lli")) {
+            allocator.free(tool);
+        }
+    };
+
     var argv = std.ArrayList([]const u8){};
     defer argv.deinit(allocator);
 
-    try argv.append(allocator, "lli");
+    try argv.append(allocator, lli_tool.?);
     try argv.append(allocator, ll_file);
 
     for (program_args) |arg| {
@@ -27,7 +41,18 @@ pub fn runWithLLI(allocator: std.mem.Allocator, ll_file: []const u8, program_arg
 }
 
 pub fn checkLLIAvailable(allocator: std.mem.Allocator) bool {
-    var child = std.process.Child.init(&[_][]const u8{ "lli", "--version" }, allocator);
+    const lli_tool = llvm_tools.getLLVMToolPath(allocator, .lli) catch return false;
+    defer if (lli_tool) |tool| {
+        if (!std.mem.eql(u8, tool, "lli")) {
+            allocator.free(tool);
+        }
+    };
+
+    if (lli_tool == null) {
+        return false;
+    }
+
+    var child = std.process.Child.init(&[_][]const u8{ lli_tool.?, "--version" }, allocator);
     child.stdin_behavior = .Ignore;
     child.stdout_behavior = .Ignore;
     child.stderr_behavior = .Ignore;
