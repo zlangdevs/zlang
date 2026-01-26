138594645be439619638802e0bfd5571ff097183
Hedgegod <hedgegod@stepbro.wtf>
2026-01-26 22:22:39 +0300

    Update build script to specify output file name

--- examples/stbds/build.sh
+++ examples/stbds/build.sh
@@ -1,2 +1,2 @@
-../../zig-out/bin/zlang wrapper.zl main.zl -link ../../thirdparties/libstb_wrapper.a 
-./output
+../../zig-out/bin/zlang wrapper.zl main.zl -link ../../thirdparties/libstb_wrapper.a -o stbds
+./stbds
